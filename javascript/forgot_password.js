80520c0c2ec8080f9faffef748b066a8b7b87ea6
harsh.sinha@hotmail.com <harsh.sinha@hotmail.com>
2020-02-19 17:54:01 -0500

    made log in page better

---
+++ javascript/forgot_password.js
@@ -0,0 +1,6 @@
+// This is file informs the user that they have properly submitted an email id to recover their password
+
+function submitButtonOnClick(id)
+{
+	alert("Your Email ID has been Submitted. You should receive an email with instructions to reset your account password.");
+}
