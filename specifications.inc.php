22f4834b7a22f6147fbb67759401099b8b04eb01
acp21h <acpohl21@gmail.com>
2020-04-12 13:56:39 -0400

    File used to store product specs

---
+++ specifications.inc.php
@@ -0,0 +1,5 @@
+<?php
+    $specs = array();
+    $specs[1] = array('Intel Core i5 10th Gen 1035G1', '8 GB LPDDR4 Memory 512 GB SSD', 'Intel UHD Graphics', '1920 x 1080 Display');
+    $specs[2] = array('Heart Rate Tracker', 'Dynamic GPS', 'Music Player', 'Makes Calls')
+?>
