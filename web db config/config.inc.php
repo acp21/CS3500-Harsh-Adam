f5c3df34186681d765ebb61994492b6d46db54b4
harsh.sinha@hotmail.com <harsh.sinha@hotmail.com>
2020-04-12 20:41:40 -0400

    added config info for web data base

---
+++ web db config/config.inc.php
@@ -0,0 +1,8 @@
+<?php 
+	//PHP File to Store Database access information
+	define('DBHOST', '');
+	define('DBNAME', 'id13256026_authorized_users');
+	define('DBUSER', 'id13256026_testuser');
+	define('DBPASS', '=N$Jw*L8E|kJZdmw');
+	define('DBCONNSTRING','mysql:dbname=id13256026_authorized_users;charset=utf8mb4;');
+?>
